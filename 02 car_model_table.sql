8a4ca39e8558bdb35d7e90c4a9dc3b813628ea08
fabio-k <fabiokmain@gmail.com>
2024-09-30 12:38:13 -0300

    add table car_model and insert some data

---
+++ 02 car_model_table.sql
@@ -0,0 +1,18 @@
+-- SQLite
+
+CREATE TABLE CAR_MODEL (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    MODEL_NAME VARCHAR(120)
+);
+
+INSERT INTO CAR_MODEL (MODEL_NAME) VALUES 
+('Conversível'),
+('Sedã'),
+('Hatch'),
+('Coupé'),
+('Perua'),
+('SUV'),
+('Picape'),
+('Minivan'),
+('Utilitário'),
+('Buggy');
