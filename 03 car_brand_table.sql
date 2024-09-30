b54e7dfca850a5b39871a7f74fa58d69564510b9
fabio-k <fabiokmain@gmail.com>
2024-09-30 12:46:28 -0300

    add table car_brand and insert some data

---
+++ 03 car_brand_table.sql
@@ -0,0 +1,16 @@
+-- SQLite
+
+CREATE TABLE CAR_BRAND (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    BRAND_NAME VARCHAR(120)
+);
+
+INSERT INTO CAR_BRAND (BRAND_NAME) VALUES 
+('Chevrolet'),
+('Toyota'),
+('Hyundai'),
+('Volkswagen'),
+('Jeep'),
+('Renault'),
+('Honda'),
+('Fiat');
